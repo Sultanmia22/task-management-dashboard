1c51c60f973424d352c1eeefdb36867ab038aabd
MD Sultan Mia <sultanmia5732@gmail.com>
2026-02-26 22:24:37 +0600

    design dashboard structure

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import DashboardSidebar from '../Components/DashboardSidebar/DashboardSidebar'
+import { HiMenuAlt3 } from 'react-icons/hi';
 
 const MainLayout = () => {
   return (
     <div>
-        <Outlet></Outlet>
+
+   <div className='flex lg:hidden'>
+     <nav className="flex items-center justify-between px-6 py-4 bg-white shadow-sm border-b w-full">
+      
+      {/* Left side: Dashboard Text */}
+      <h1 className="text-xl font-bold text-gray-800">
+        Dashboard
+      </h1>
+
+      {/* Right side: Sidebar Icon */}
+      <button className="p-2 hover:bg-gray-100 rounded-lg transition-colors">
+        <HiMenuAlt3 className="text-2xl text-gray-700" />
+      </button>
+
+    </nav>
+   </div>
+
+      <div className="grid lg:grid-cols-12 min-h-screen gap-5 p-5">
+
+        {/* Sidebar */}
+        <div className="hidden lg:block col-span-3 w-full">
+          <DashboardSidebar />
+        </div>
+
+        {/* Main content */}
+        <div className="col-span-12 lg:col-span-9 w-full">
+          <Outlet />
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/page/Dashboard/Dashboard.jsx
+++ src/page/Dashboard/Dashboard.jsx
@@ -2,7 +2,54 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+       <div className="bg-gray-100 min-h-screen p-6 flex flex-col gap-5">
+
+        <div className='border-2 border-red-500 p-5'>
+
+        </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 w-full">
+
+
+        <div className="border-2 border-red-500 p-4 rounded-xl">Total Projects</div>
+
+        <div className="border-2 border-red-500 p-4 rounded-xl">Ended Projects</div>
+
+        <div className="border-2 border-red-500 p-4 rounded-xl">Running Projects</div>
+
+        <div className="border-2 border-red-500 p-4 rounded-xl">Pending Project</div>
+
+
+
+       
+
+        <div className="border-2 border-red-500 p-4 rounded-xl lg:col-span-2 row-span-2">Project Analytics</div>
+
+        <div className="border-2 border-red-500 p-4 rounded-xl lg:row-span-2">Reminder</div>
+
+        <div className="border-2 border-red-500 p-4 rounded-xl lg:row-span-3">Project</div>
+
+
+
+      
+
+
+
+        
+
+        <div className="border-2 border-red-500 p-4 rounded-xl lg:col-span-2 row-span-2">Team Collaboration</div>
+
+        <div className="border-2 border-red-500 p-4 rounded-xl lg:row-span-2">Project Progress</div>
+
+        <div className="border-2 border-red-500 p-4 rounded-xl">Time Tracker</div>
+
+
+
+      </div>
+
+  </div>
+    </div>
   )
 }
 
